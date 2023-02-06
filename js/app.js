d6422a623d811164eaaa9f846eac85ea6ae7a94d
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-06 14:50:14 +0100

    EDIT+STYLE creato CSS

---
+++ js/app.js
@@ -0,0 +1,11 @@
+Consegna:
+Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+In questo esercizio è importante seguire le milestone per affrontare in maniera via via crescente le difficoltà di ogni problema.
+MILESTONE 1
+Per prima cosa, creiamo il markup statico in modo da avere così la struttura base e gli stili pronti per poterci poi concentrare solamente sull’aspetto logico.
+Non siete obbligati ad usare lo stesso markup visto a lezione, potete usare anche una versione più snella o anche più articolata. Ma fate attenzione che alla milestone 3 questo markup sarà da generare con javascript.:occhiolino:
+MILESTONE 2
+Al click dell’utente sulle frecce (una a sinistra e una a destra), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. Attenzione quando arriviamo all’ultima slide e clicchiamo sulla freccia destra, o viceversa se siamo sulla prima e clicchiamo sulla freccia sinistra.
+MILESTONE 3
+Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell’array fornito per generare il markup delle slide potete usare la concatenazione delle stringe, il template literal o il metodo createElement… a voi la scelta.
+Nel generare il murkup fate attenzione che solo una delle 5 slide deve avere la classe active e risultare quindi visibile…
