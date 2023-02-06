9da1d40c3ad8029f38c05bb2020e8445023d167d
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-06 15:34:06 +0100

    EDIT aggiunta funzione js preview

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 //MILESTONE 2:Al click dell’utente sulle frecce (una a sinistra e una a destra), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. 
 
+//MILESTONE 2:Al click dell’utente sulle frecce (una a sinistra e una a destra), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. 
+
 
 //1. Dichiariamo le constanti slide, arrow e current slide del nostro carosello
 const slides = document.querySelectorAll('.slide');
 const leftArrow = document.querySelector('.arrow-left');
 const rightArrow = document.querySelector('.arrow-right');
 let currentSlide = 0;
+
+//2. Resetto le slide con for per dare display none
+function reset() {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+  }
+
+//3. Diamo display block allo slideshow
+function startSlide() {
+    reset();
+    slides[0].style.display = 'block';
+  }  
+
+
+ //4. Preview delle slide
+function prevSlide() {
+    reset();
+    slides[currentSlide - 1].style.display = 'block';
+    currentSlide--;
+  } 
 
